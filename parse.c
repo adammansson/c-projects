8fc5a364911221f0f6018cae5b716e5bf98a1247
Adam Månsson <adam.s.mansson@gmail.com>
2022-04-14 22:14:54 +0200

    Add parse file

---
+++ parse.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int intexp(const int base, int n)
+{
+    int i, p = 1;
+
+    for (i = 1; i < n; i++)
+    {
+        p *= base;
+    }
+    return p;
+}
+
+
+int parse(char* str)
+{
+    int num, i;
+    int len = strlen(str);
+    
+    for (i = 0; i < len; i++)
+    {
+        int parsed = str[i] - '0';
+        num += parsed * intexp(10, len - i);
+    }
+    return num;
+}
+
+
+int main(int argc, char* argv[])
+{
+    printf("%i\n", parse(argv[1]));
+    return 0;
+}
+
